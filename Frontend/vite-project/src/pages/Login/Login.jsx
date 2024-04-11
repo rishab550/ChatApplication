888997fd74f6bae0a287e3c113c116d59745c78e
Rishabh Garg <you@example.com>
2024-04-11 13:20:02 +0530

    ok

--- Frontend/vite-project/src/pages/Login/Login.jsx
+++ Frontend/vite-project/src/pages/Login/Login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
               />
             </div>
             <Link
-              to="http://localhost:3000/sign-up"
+              to="http://localhost:5000/sign-up"
               className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
             >
               Don't have an account

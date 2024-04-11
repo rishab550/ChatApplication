e7e5590065788732888c50626b1abba1225ee16b
Rishabh Garg <you@example.com>
2024-04-11 12:40:00 +0530

    Production complete

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { app } from "./socket/socket.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -15,6 +18,8 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, "/frontend/vite-project/dist")));
+
 // routes import
 
 import router from "./routes/user.route.js";
@@ -26,5 +31,10 @@ import activeUserRouter from "./routes/activeUsers.route.js";
 app.use("/api/v1/users", router);
 app.use("/api/v1/messages", messageRouter);
 app.use("/api/v1/active-users", activeUserRouter);
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "frontend", "vite-project", "dist", "index.html"),
+  );
+});
 
 export default app;
